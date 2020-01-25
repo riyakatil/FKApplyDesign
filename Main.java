e4a1d1d89da6084a1bbbc1a731ee73e821758103
Riya <Riyakathil1993@gmail.com>
2020-01-25 12:39:26 +0530

    Changes to be committed: 	new file:   .gitignore 	new file:   Main.java 1. tile is basic unit ,interface is tile and singleTile is class implementing it 2.tileEventHandlerManager is interface which takes care when one tries to access any tile, tileEventHandler is class implementing it
    3.BoardInterface is collection of tiles and board is implementing that interface
    4. gameManger is first inteface player will taklk to, it will configure the game
    5. ConfigurationManager takes care of the game

---
+++ Main.java
@@ -0,0 +1,226 @@
+//package com.company;
+
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.HashMap;
+import  java.util.*;
+import java.io.*;
+
+//user input
+interface userInput{
+    String getInput();
+   // int convertToInt(String s);
+    //String convertToDouble(String s);
+}
+
+class HandleUserInput implements  userInput{
+    private Scanner obj;
+    HandleUserInput(){ obj=new Scanner((System.in));}
+
+   public String getInput(){return obj.next();}
+
+
+}
+
+
+//Tiles
+interface tile{
+
+    boolean getStatus();
+    void changeStatus();
+}
+
+class singleTile implements tile{
+    private ArrayList<Integer> dimension;
+    private  boolean status;
+    singleTile(int ...l1){
+        int num=l1.length;
+        dimension=new ArrayList<>(Collections.nCopies(num,0));
+        this.status=false;
+    }
+ public   boolean getStatus(){
+        return this.status;
+    }
+
+public  void changeStatus(){
+        this.status=true;
+    }
+
+}
+
+
+//Tile event handler
+interface tileEventHandlerManager{
+void changeStatus(tile t1);
+boolean getStatus(tile t1);
+}
+
+
+
+class tileEventHandler implements  tileEventHandlerManager{
+ public void changeStatus(tile t1){
+     t1.changeStatus();
+ }
+ public boolean getStatus(tile t1){
+return t1.getStatus();
+ }
+}
+
+
+//Board
+
+
+interface boardInterface{
+    boolean isEmpty(Board b);
+    boolean isFull(Board b);
+    void changeStatus(Board b,tile t1);
+   // Board getBoard(int n,int m);
+}
+
+class Board implements  boardInterface{
+    private ArrayList<ArrayList<singleTile>> arr;
+    private int row;
+    private int col;
+    private tileEventHandlerManager tileEHM;
+    private static Board b1=null;
+
+    //private constructor becuase this is singleton class
+    private Board(int n,int m){
+        arr = new ArrayList<ArrayList<singleTile>>(n);
+        for(int i=0;i<n;i++) arr.set(i, new ArrayList<>(m));
+      tileEHM =new tileEventHandler();
+    }
+
+
+
+    //get Board method
+    public static Board getBoard(int n,int m){
+       if(Objects.isNull(b1)){ b1=new Board(n,m); }
+       return b1;
+    }
+   public boolean isEmpty(Board b){
+
+   for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){ if(b.tileEHM.getStatus(arr.get(i).get(j))==false) {return  true; };
+   }
+    }
+   return false;
+
+}
+    public boolean isFull(Board b){
+
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){ if(b.tileEHM.getStatus(arr.get(i).get(j))) {return  false; };
+            }
+        }
+        return true;
+
+    }
+
+
+    public void changeStatus(Board b,tile t1){
+        b.tileEHM.changeStatus(t1);
+
+    }
+
+}
+
+
+
+/*
+
+class boardManager implements boardManagerInterface{
+    private userInput obj;
+    private boardInterface boardTemplate;
+    boardManager(){
+        obj=new handleUserInput();
+
+
+    }
+    public void configureGame(){
+
+        System.out.println("Enter number of rows in board");
+        int rows=Integer.parseInt(obj.getInput());
+
+        System.out.println("Enter number of columns in board");
+        int cols=Integer.parseInt(obj.getInput());
+        boardTemplate=new Board();
+
+    }
+
+    public void eventManager(){}
+}
+/*
+class board implements  boardManager{
+    private int rows,cols;
+
+    board(int a=3,int b=3){
+        this.rows=a;
+        this.cols=b;
+    }
+
+
+ public    int getRows() { return this.rows; }
+   public  int getCols() { return this.cols; }
+}
+
+
+interface playerType{
+    void getPlayerType();
+}
+*/
+
+//first interface user will contact with this interface only to start the game.
+interface gameManagerInterface{
+    void startNewGame(gameManager gm1);
+    void exitGame();
+ //   gameManager getGameManager();
+}
+
+class gameManager implements gameManagerInterface{
+    private configurationManagerInterface configMI;
+    private static gameManager gm1=null;
+    private gameManager(){
+
+    }
+    public static gameManager getGameManager(){
+            if(gm1==null){gm1=new gameManager();}
+            return gm1;
+    }
+
+    public void startNewGame(gameManager gm1){
+                    gm1.configMI.startGame();
+    }
+
+    public void exitGame(){System.exit(0);}
+
+}
+
+//second interface  (game manager will talk to config manager)
+interface configurationManagerInterface{
+    void configureGame();
+    void startGame();
+    void exitGame();
+  //  boardManager getBoard(); //BoardManager
+    //void getRules(); //JudgeManager
+    //void getPlayerTeam();  // playerTeamManager
+    //void playGame(); //PlayGameManager  -- start game
+
+}
+
+
+interface  playGameManager
+{
+
+    String getTurn();
+    void startGame();
+}
+
+
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
